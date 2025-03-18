a60286a41f3cc69bd831b5a8e60da7502c238f32
mystogan099 <hrithiky328@gmail.com>
2025-03-18 06:33:06 +0530

    Add ESLint configuration and ignore files: Introduce .eslintrc.json for custom ESLint rules, create .eslintignore to exclude build outputs and dependencies, and update next.config.js to allow builds with ESLint errors.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({ 
+    extends: ["./.eslintrc.json"] 
+  })
 ];
 
 export default eslintConfig;
